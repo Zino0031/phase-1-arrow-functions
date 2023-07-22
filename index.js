a0269cfbb18a17b48034d1da481a3ff9beccf769
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-07-22 16:51:09 +0200

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function (){
+    return 2000 / 100;
+    
+
+}
+
+const square = (parameter, parameter2) => parameter * parameter;
+
+
+   
+const add = (parameter, parameter2) => parameter + parameter2;
+
+   
